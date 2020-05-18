e7171e12766f486ed0ca99e78f02db488ab08646
laura mairot <lauramairot@gmail.com>
2020-05-18 20:54:52 +0200

    add_root_to_routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'cocktails#index'
   get    "cocktails",          to: "cocktails#index"
   get    "cocktails/new",      to: "cocktails#new", as: :new_cocktail
   post   "cocktails",          to: "cocktails#create"
